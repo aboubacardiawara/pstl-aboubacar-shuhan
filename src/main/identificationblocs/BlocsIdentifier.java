b7acabc2031cb79ec0590fcbeea4e2b88f4bcd51
waradia <aboubacardiawara909@gmail.com>
2023-04-17 22:28:38 +0200

    init bloc identification

---
+++ src/main/identificationblocs/BlocsIdentifier.java
@@ -0,0 +1,15 @@
+package main.identificationblocs;
+
+import main.adaptation.interfaces.IRUAST;
+
+public class BlocsIdentifier {
+    
+    /**
+     * Prend un arbre RUAST et identifie les noeuds auxquels les blocs appartiennent.
+     * @param ruast
+     * @return
+     */
+    public IRUAST process(IRUAST ruast) {
+        return null;
+    }
+}
