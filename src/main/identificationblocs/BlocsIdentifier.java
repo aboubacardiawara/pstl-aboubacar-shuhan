3413f6191b61fc913cc9adfe5cd9134513bd0097
waradia <aboubacardiawara909@gmail.com>
2023-04-18 14:05:45 +0200

    update

--- src/main/identificationblocs/BlocsIdentifier.java
+++ src/main/identificationblocs/BlocsIdentifier.java
@@ -10,65 +10,6 @@ public class BlocsIdentifier {
 
     private Integer id = 1;
 
-    /*
-     * 
-     * /*
-     * Prend un arbre RUAST et identifie les noeuds auxquels les blocs
-     * appartiennent.
-     * 
-     * @param ruast
-     * 
-     * @return
-     * /
-     * public IRUAST process(IRUAST ruast) {
-     * HashMap<VariantsSet, Integer> block_infos = new HashMap<>();
-     * IRUASTNode root = ruast.getRoot();
-     * aux_node(ruast, block_infos, root);
-     * return ruast;
-     * }
-     * 
-     * private void aux_node(IRUAST ruast, HashMap<VariantsSet, Integer>
-     * block_infos, IRUASTNode node) {
-     * VariantsSet block = node.getVariants();
-     * if (block_infos.containsKey(block)) {
-     * int id_block = block_infos.get(block);
-     * node.setBlock(id_block);
-     * } else {
-     * block_infos.put(block, getNextIdForBloc());
-     * node.setBlock(getNextIdForBloc());
-     * for (VariantsSet existing_block : block_infos.keySet()) {
-     * // dependence
-     * if (subset(block, existing_block)) {
-     * 
-     * }
-     * // mutex
-     * if (intersection(block, existing_block).isEmpty()) {
-     * 
-     * }
-     * }
-     * }
-     * for (IRUAST child : ruast.getChildren()) {
-     * aux_node(child, block_infos, child.getRoot());
-     * }
-     * 
-     * }
-     * 
-     * 
-     * private boolean subset(VariantsSet a, VariantsSet b) {
-     * return b.containsAll(a);
-     * }
-     * 
-     * private VariantsSet intersection(VariantsSet a, VariantsSet b) {
-     * VariantsSet result = new VariantsSet();
-     * for (String variant : a) {
-     * if (b.contains(variant)) {
-     * result.add(variant);
-     * }
-     * }
-     * return result;
-     * }
-     */
-
     private Integer getNextIdForBloc() {
         return id++;
     }
@@ -76,7 +17,7 @@ public class BlocsIdentifier {
     public IRUAST findBlocs(IRUAST ruast) {
         Map<Set<Integer>, Integer> env = new HashMap<>();
         findBlocsAux(ruast, env);
-        System.out.println("blocs: " + env);
+        System.out.println("Features[" + env.size() + "]: " + env);
         return ruast;
     }
 
