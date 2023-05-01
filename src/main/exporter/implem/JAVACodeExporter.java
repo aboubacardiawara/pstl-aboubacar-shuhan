24059f59588eac84b6bfc7d299df384f2e841858
waradia <aboubacardiawara909@gmail.com>
2023-05-01 15:28:53 +0200

    can create file

--- src/main/exporter/implem/JAVACodeExporter.java
+++ src/main/exporter/implem/JAVACodeExporter.java
@@ -1,11 +1,14 @@
 package main.exporter.implem;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import org.eclipse.core.internal.resources.Folder;
-import org.eclipse.core.runtime.Path;
+import java.nio.file.Path;
 
+import main.adaptation.RUASTNodeType;
 import main.adaptation.interfaces.IRUAST;
 import main.exporter.IExporter;
 
@@ -20,20 +23,34 @@ public class JAVACodeExporter implements IExporter {
 
     @Override
     public void export(IRUAST ruast) {
-        createOnlyClassFiles();
+        createOnlyClassFiles(ruast);
     }
 
     /**
      * 
      */
-    private void createOnlyClassFiles() {
+    private void createOnlyClassFiles(IRUAST ruast) {
         createFolder();
-
+        createFileFromRUAST(ruast.getChildren().get(0));
     }
 
+    private void createFileFromRUAST(IRUAST ruast) {
+        assert ruast.getRoot().getType() == RUASTNodeType.TYPE_DEFINITION : "Should be a type definition";
+        String fileName = "RandomJavaClass.java";
+        Path filePath = Paths.get(this.folderPath + "/" + fileName);
+        try {
+            Files.createFile(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Create folder and juste files
+     */
     private void createFolder() {
         try {
-            java.nio.file.Path path = Paths.get(this.folderPath);
+            Path path = Paths.get(this.folderPath);
             Files.createDirectory(path);
         } catch (Exception e) {
             e.getStackTrace();
